2352556ee0596f548e79a7ab83026d0b5368732f
0vishal <vishalsalaskar72@gmail.com>
2021-02-25 09:11:58 +0530

    switch case

--- EmpwageBuilder.java
+++ EmpwageBuilder.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 public class EmpwageBuilder {
 
     public static void main(String[] args) {
-       
         // write your code here
 
         System.out.println("Welcome to Employee wage computation problem on master branch");
-        System.out.println("Calculate wage 1.FullTime 2.PartTime");
-        Scanner scan = new Scanner(System.in);
-        int TIME = scan.nextInt();
 
         Employee emp = new Employee();
 
@@ -21,21 +17,25 @@ public class EmpwageBuilder {
         class Employee {
             int EMPWAGEPERHOUR;
             int EMPTIME;
-            int TIME;
-            int DAILYWAGE;
 
             void wage() {
-                int EMPTIME;
+                System.out.println("Calculate wage 1.FullTime 2.PartTime");
+                Scanner scan = new Scanner(System.in);
+                int TIME = scan.nextInt();
 
-                if ( TIME == 1 )  {
-                    EMPTIME = 8;
-                } else {
-                    EMPTIME = 4;
+                switch (TIME) {
+                    case 1:
+                        EMPTIME = 8;
+                        break;
+                    case 2:
+                        EMPTIME = 4;
+                        break;
                 }
                 int DAILYWAGE = (EMPTIME * EMPWAGEPERHOUR);
                 System.out.println("Daily wage is " + DAILYWAGE);
             }
         }
+
 
 
 
