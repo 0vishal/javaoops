15cd4b2e71b973c7eb8c0a0c4ce52b2a1383af2f
0vishal <vishalsalaskar72@gmail.com>
2021-02-25 23:58:03 +0530

    check equal

--- Linecompare.java
+++ Linecompare.java
@@ -2,26 +2,51 @@ import java.util.Scanner;
 
 public class Linecompare {
 
+   import java.util.*;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class LinecomparisonUC1 {
+
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Welcome to line comparison computation program on master branch");
-        System.out.println("We will calculate length of line x1 y1 x2 y2 ");
-        Scanner scan = new Scanner(System.in);
+        System.out.println("We will calculate length of line s1 t1 s2 t2 ");
+
 
         Linecalculate lineone = new Linecalculate();
-        lineone.x1 = scan.nextInt();
-        lineone.y1 = scan.nextInt();
-        lineone.x2 = scan.nextInt();
-        lineone.y2 = scan.nextInt();
-        lineone.Linecalculate();
+        System.out.println(lineone.calculate());
+
+
+        System.out.println("We will calculate length of line p1 q1 p2 q2 ");
+        Linecalculate linetwo = new Linecalculate();
+        System.out.println(linetwo.calculate());
+
+        
+        int lengthL1= lineone.calculate();
+        int lengthL2 = linetwo.calculate();
+
+        int Equals= lengthL1.equals(lengthL2);
+        if (Equals== True){
+            System.out.println("Lines are equal ");
+        }
+        else
+            System.out.println("Lines are not equal");
+
     }
-}
+    }
 
 class Linecalculate {
-    int x1, y1, x2, y2;
 
-    void Linecalculate() {
-        double len = Math.sqrt((x2-x1)^2+(y2-y1)^2);
-        System.out.println(len);
+        public int calculate() {
+
+            Scanner scan = new Scanner(System.in);
+            int x1 = scan.nextInt();
+            int y1 = scan.nextInt();
+            int x2 = scan.nextInt();
+            int y2 = scan.nextInt();
+            int len = (int)Math.floor(Math.sqrt((x2-x1)^2+(y2-y1)^2));
+            System.out.println(len);
+            return len;
     }
 }
